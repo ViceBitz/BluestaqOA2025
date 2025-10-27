db546fb9dac75cfa29c628aea5ed039182e68b98
ViceBitz <25victorg@students.harker.org>
2025-10-26 23:11:21 -0400

    run elevator

--- SimpleElevatorMVP/main.java
+++ SimpleElevatorMVP/main.java
@@ -1,7 +1,11 @@
 package SimpleElevatorMVP;
 
+/**
+ * Handles listening to user inputs and sending actions to elevator
+ */
 class Main {
     public static void main(String args[]) {
-        
+        Elevator elevator = new Elevator();
+        elevator.start();
     }
 }
